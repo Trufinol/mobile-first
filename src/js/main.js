8accca907959fe4fc882879ccea99905a0f53710
nikolay <nv@pmgnet.de>
2017-09-11 21:05:05 +0300

    add new pages

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,12 @@
-console.log('ping!')
+$(function() {
+    // Close modal
+    $('.modal-close').click(function(e) {
+        e.preventDefault();
+        console.log($(this).parent('.modal').toggleClass('is-active'));
+    });
+
+    // Close message
+    $(document).on('click', '.message-panel .dismiss', function() {
+        $(this).parent('.message-panel').fadeOut(300);
+    });
+});
